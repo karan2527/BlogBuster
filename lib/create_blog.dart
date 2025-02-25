62363113e351810492795368269b432e6717045c
karan2527 <karanjain2527@gmail.com>
2025-02-26 01:42:17 +0530

    pages created

---
+++ lib/create_blog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateBlog extends StatefulWidget {
+  const CreateBlog({super.key});
+
+  @override
+  State<CreateBlog> createState() => _CreateBlogState();
+}
+
+class _CreateBlogState extends State<CreateBlog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
