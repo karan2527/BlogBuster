814784de6f625c1a1b9deb953020f0524ea46535
karan2527 <karanjain2527@gmail.com>
2025-02-26 09:56:59 +0530

    floating action button-Add

--- lib/home.dart
+++ lib/home.dart
@@ -31,6 +31,15 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         elevation: 0.0,
       ),
+      body: Container(),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          FloatingActionButton(onPressed: (){},
+          child: Icon(Icons.add),
+          )
+        ],
+      ),
     );
   }
 }
